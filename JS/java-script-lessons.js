f94642d80873a104d007c891cf15f0c0b3ca3712
bogfanof <sano.tsarevich@gmail.com>
2024-07-22 20:12:13 +0300

    Third Commit

--- JS/java-script-lessons.js
+++ JS/java-script-lessons.js
@@ -2,3 +2,12 @@ let num = 122;
 
 (num === 11) ? console.log('Правильно') : console.log('Увы');
 
+const abc = 3; zxc = 4; qwerty = 1;
+
+if (abc && qwerty) {
+    console.log('QWer')
+} else {
+    console.log('rewq')
+};
+
+
