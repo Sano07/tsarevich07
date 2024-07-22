8731dfa1fc01ce7ae032af89c0a7740286bd0b7c
bogfanof <sano.tsarevich@gmail.com>
2024-07-22 22:23:56 +0300

    4 commit cycle

--- JS/java-script-lessons.js
+++ JS/java-script-lessons.js
@@ -1,7 +1,10 @@
+// Краткая запись условия - условие ? если да : если нет
+
 let num = 122;
 
 (num === 11) ? console.log('Правильно') : console.log('Увы');
 
+// запись условия стандартным способом 
 const abc = 3; zxc = 4; qwerty = 1;
 
 if (abc && qwerty) {
@@ -10,4 +13,27 @@ if (abc && qwerty) {
     console.log('rewq')
 };
 
+
+// Циклы
+
+let num1 = 66;
+
+// while (num1 <= 70) {
+//     console.log(num1);
+//     num1++;
+// }
+
+// do {
+//     console.log(num1);
+//     num1++;
+// }
+// while (num1 <= 70)
+
+// for (let i = 1; i < 8; i++) {
+//     if (i === 7) {
+//         break;
+//         //continue  -- позволяет пропустить итерацию цикла
+//     }
+//     console.log(i);
+// }
 
