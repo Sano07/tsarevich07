2d24f88ebda7971f913fa9d3d2c2caea3594dace
bogfanof <sano.tsarevich@gmail.com>
2024-07-23 21:38:55 +0300

    Five commit metkiii

--- JS/java-script-lessons.js
+++ JS/java-script-lessons.js
@@ -1,22 +1,22 @@
-// Краткая запись условия - условие ? если да : если нет
+ КРАТКАЯ ЗАПИСЬ УСЛОВИЯ = УСЛОВИЕ ? ЧТО ЕСЛИ ДА : ЧТО ЕСЛИ НЕТ ******************************
 
-let num = 122;
+// let num = 122;
 
-(num === 11) ? console.log('Правильно') : console.log('Увы');
+// (num === 11) ? console.log('Правильно') : console.log('Увы');
 
-// запись условия стандартным способом 
-const abc = 3; zxc = 4; qwerty = 1;
+ ЗАПИСЬ УСЛОВИЯ СТАНДАРТНЫМ СПОСОБОМ ******************************************************** 
+// const abc = 3; zxc = 4; qwerty = 1;
 
-if (abc && qwerty) {
-    console.log('QWer')
-} else {
-    console.log('rewq')
-};
+// if (abc && qwerty) {
+//     console.log('QWer')
+// } else {
+//     console.log('rewq')
+// };
 
 
-// Циклы
+ ЦИКЛЫ **************************************************************************************
 
-let num1 = 66;
+// let num1 = 66;
 
 // while (num1 <= 70) {
 //     console.log(num1);
@@ -37,3 +37,42 @@ let num1 = 66;
 //     console.log(i);
 // }
 
+ ЦИКЛ В ЦИКЛЕ *******************************************************************************
+
+// for (let i = 1; i < 8; i++) {
+//     for (let j = 1; j < 8; j++) {
+//         console.log(j);
+//     };
+//     console.log(i);
+// };
+
+ПОСТРОЕНИЕ ТРЕУГОЛЬНИКА +  *****************************************************************
+
+// let result = '';
+//     length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+МЕТКИ 
+
+// first: for (let i = 1; i < 3; i++) {
+//     console.log(`First cycle: ${i}`);
+//     for (let j = 1; j < 3; j++) {
+//         console.log(`Second cycle: ${j}`);
+//         for (let k = 1; k < 3; k++) {
+//             console.log(`Third cycle: ${k}`);
+//             if (k === 2) continue first;
+//         };
+//     };
+// };
+asd
